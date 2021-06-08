7709dbf6b4775ff0bb38468cce761f38a6500c31
ethanhousley <66891702+ethanhousley@users.noreply.github.com>
2021-06-08 14:25:59 -0500

    Add project files.

---
+++ DBCrud/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace DBCrud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBxUserID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBxState = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBxCity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBxZip = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBxUserID
+            // 
+            this.txtBxUserID.Location = new System.Drawing.Point(41, 82);
+            this.txtBxUserID.Name = "txtBxUserID";
+            this.txtBxUserID.Size = new System.Drawing.Size(105, 20);
+            this.txtBxUserID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "UserID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "State";
+            // 
+            // txtBxState
+            // 
+            this.txtBxState.Location = new System.Drawing.Point(41, 140);
+            this.txtBxState.Name = "txtBxState";
+            this.txtBxState.Size = new System.Drawing.Size(105, 20);
+            this.txtBxState.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "City";
+            // 
+            // txtBxCity
+            // 
+            this.txtBxCity.Location = new System.Drawing.Point(41, 196);
+            this.txtBxCity.Name = "txtBxCity";
+            this.txtBxCity.Size = new System.Drawing.Size(105, 20);
+            this.txtBxCity.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Zip Code";
+            // 
+            // txtBxZip
+            // 
+            this.txtBxZip.Location = new System.Drawing.Point(41, 255);
+            this.txtBxZip.Name = "txtBxZip";
+            this.txtBxZip.Size = new System.Drawing.Size(105, 20);
+            this.txtBxZip.TabIndex = 6;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(209, 66);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(93, 36);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(209, 124);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(93, 36);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(209, 180);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(93, 36);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(209, 239);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(93, 36);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 331);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBxZip);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBxCity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBxState);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBxUserID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBxUserID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBxState;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBxCity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBxZip;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ DBCrud/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBCrud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using (FirstDatabaseEntities _dbContext = new FirstDatabaseEntities())
+            {
+
+                var updateUser = new PrimaryUser();
+                updateUser.UserID = Int32.Parse(txtBxUserID.Text);
+                updateUser.State = txtBxState.Text;
+                updateUser.City = txtBxCity.Text;
+                updateUser.Zip = Int32.Parse(txtBxZip.Text);
+
+                _dbContext.Entry(updateUser).State = System.Data.Entity.EntityState.Modified;
+                _dbContext.SaveChanges();
+                MessageBox.Show("Successfully Updated User.");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (FirstDatabaseEntities _dbContext = new FirstDatabaseEntities())
+            {
+                if (Validation())
+                {
+                    var newUser = new PrimaryUser();
+                    newUser.UserID = Int32.Parse(txtBxUserID.Text);
+                    newUser.State = txtBxState.Text;
+                    newUser.City = txtBxCity.Text;
+                    newUser.Zip = Int32.Parse(txtBxZip.Text);
+
+                    _dbContext.PrimaryUsers.Add(newUser);
+                    _dbContext.SaveChanges();
+                    MessageBox.Show("New User Saved.");
+
+                }
+
+                else
+                {
+                    MessageBox.Show("User ID field must be populated.");
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using(FirstDatabaseEntities _dbContext = new FirstDatabaseEntities())
+            {
+                int U = int.Parse(txtBxUserID.Text);
+                var delUser = _dbContext.PrimaryUsers.First(x => x.UserID == U);
+
+
+                _dbContext.PrimaryUsers.Remove(delUser);
+                _dbContext.SaveChanges();
+                MessageBox.Show("User Successfully Deleted.");
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            using(FirstDatabaseEntities _dbContext = new FirstDatabaseEntities())
+            {
+
+                int U = int.Parse(txtBxUserID.Text);
+                var searchUser = _dbContext.PrimaryUsers.First(x => x.UserID == U);
+
+                txtBxUserID.Text = searchUser.UserID.ToString();
+                txtBxCity.Text = searchUser.City;
+                txtBxState.Text = searchUser.State;
+                txtBxZip.Text = searchUser.Zip.ToString();
+
+                MessageBox.Show("User Found.");
+
+            }
+        }
+
+
+        public bool Validation()
+        {
+
+            if (string.IsNullOrWhiteSpace(txtBxUserID.Text))
+            {
+
+                return false;
+
+            }
+
+            return true;
+            
+
+        }
+    }
+}
